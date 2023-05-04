5948c80a35fb0fb4ee2958a16656caf0fb2da582
Lucio Alconchel <lucioalconchelsj@gmail.com>
2023-05-04 18:51:45 -0300

    Trabajo Final trayecto 1 Argentina Programa

---
+++ Proyecto/src/main/java/Classes/Equipo.java
@@ -0,0 +1,30 @@
+package Classes;
+
+public class Equipo {
+
+    private String nombre;
+
+    private String descripcion;
+
+    public Equipo(String nombre, String descripcion) {
+        this.setearNombre(nombre);
+        this.setearDescripcion(descripcion);
+    }
+
+
+    public String obtenerNombre() {
+        return this.nombre;
+    }
+    public String obtenerDescripcion(){
+        return this.descripcion;
+    }
+
+    public void setearNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setearDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
